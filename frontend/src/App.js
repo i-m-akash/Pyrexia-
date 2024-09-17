e3fe83680f9fbc333cac2e01f0d86da170745696
Akash kumar <109230614+i-m-akash@users.noreply.github.com>
2024-09-17 16:15:44 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,28 +2,28 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import { AuthProvider } from './components/AuthContext';
 import ScrollToTop from './components/ScrollToTop';
-// import Login from './LoginandSignup/login';
-// import Signup from './LoginandSignup/signup';
-// import Home from './components/Home';
-// import Notfound from './LoginandSignup/notfound';
-// import ForgotPassword from './LoginandSignup/forgotpassword';
-// import ResetPassword from './LoginandSignup/resetpassword';
-// import EmailVerification from './LoginandSignup/emailverification';
+import Login from './LoginandSignup/login';
+import Signup from './LoginandSignup/signup';
+import Home from './components/Home';
+import Notfound from './LoginandSignup/notfound';
+import ForgotPassword from './LoginandSignup/forgotpassword';
+import ResetPassword from './LoginandSignup/resetpassword';
+import EmailVerification from './LoginandSignup/emailverification';
 
-// import ProtectedRoute from './components/protectedroute';
-// import Profile from './components/profile';
+import ProtectedRoute from './components/protectedroute';
+import Profile from './components/profile';
 
-// import './index.css';
+import './index.css';
 import RegisterSoon from './Events/RegisterSoon';
-// import PaymentSuccess from "./Payment/PaymentSuccess";
-// import Payment from "./Payment/Payment";
-// import EventPage from './Events/EventPage';
-// import SubEventDetails from './Events/SubEventDetails';
-// import Navbar from './components/navbar';
-// import StarNight from './components/StarNight';
-// import Legals from './Legals/Legals';
-// import Schedule from './components/Schedule';
-// import BasicRegistration from './components/BasicRegistration';
+import PaymentSuccess from "./Payment/PaymentSuccess";
+import Payment from "./Payment/Payment";
+import EventPage from './Events/EventPage';
+import SubEventDetails from './Events/SubEventDetails';
+import Navbar from './components/navbar';
+import StarNight from './components/StarNight';
+import Legals from './Legals/Legals';
+import Schedule from './components/Schedule';
+import BasicRegistration from './components/BasicRegistration';
 
 function App() {
   return (
@@ -31,29 +31,29 @@ function App() {
 
       <Router>
       <ScrollToTop/>
-        // <Navbar />
+        <Navbar />
         <Routes>
-          // <Route path='/' element={<Home />}></Route>
-          // <Route path='/login' element={<Login />}></Route>
-          // <Route path='/signup' element={<Signup />}></Route>
-          // <Route path='/notfound' element={<Notfound />}></Route>
-          // <Route path='/forgotpassword' element={<ForgotPassword />}></Route>
-          // <Route path='/resetpassword' element={<ResetPassword />}></Route>
-          // <Route path='/emailverification' element={<EmailVerification />}></Route>
-          // <Route path="/events" element={<EventPage />}></Route>
-          // <Route path="/profile" element={<ProtectedRoute element={Profile} />} />
-          // <Route path="/registerevent" element={<RegisterSoon} />}></Route>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/login' element={<Login />}></Route>
+          <Route path='/signup' element={<Signup />}></Route>
+          <Route path='/notfound' element={<Notfound />}></Route>
+          <Route path='/forgotpassword' element={<ForgotPassword />}></Route>
+          <Route path='/resetpassword' element={<ResetPassword />}></Route>
+          <Route path='/emailverification' element={<EmailVerification />}></Route>
+          <Route path="/events" element={<EventPage />}></Route>
+          <Route path="/profile" element={<ProtectedRoute element={Profile} />} />
+          <Route path="/registerevent" element={<RegisterSoon} />}></Route>
    <Route path="/" element={<RegisterSoon} />}></Route>
 
 
-          // <Route path="/paymentsuccess" element={<PaymentSuccess />} />
-          // <Route path="/payment" element={<Payment />} />
-          // <Route path="/starnight" element={<StarNight />} />
-          // <Route path="/schedule" element={<Schedule />} />
-          // <Route path="/basic-registration" element={<BasicRegistration />} />
+          <Route path="/paymentsuccess" element={<PaymentSuccess />} />
+          <Route path="/payment" element={<Payment />} />
+          <Route path="/starnight" element={<StarNight />} />
+          <Route path="/schedule" element={<Schedule />} />
+          <Route path="/basic-registration" element={<BasicRegistration />} />
 
-          // <Route path="/legalS/:pageName" element={<Legals />} />
-          // <Route path="/subevent-details" element={<SubEventDetails />}></Route>
+          <Route path="/legalS/:pageName" element={<Legals />} />
+          <Route path="/subevent-details" element={<SubEventDetails />}></Route>
         </Routes>
       </Router>
     </AuthProvider>
